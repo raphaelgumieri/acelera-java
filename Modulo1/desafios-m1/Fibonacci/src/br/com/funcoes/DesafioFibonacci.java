1c5f41ed087b113552dde4246a73f8d67a25adb9
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-10-18 16:28:52 -0300

    desafio fibonacci

---
+++ Modulo1/desafios-m1/Fibonacci/src/br/com/funcoes/DesafioFibonacci.java
@@ -0,0 +1,35 @@
+package br.com.funcoes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DesafioFibonacci {
+  public static void main(String[] args) {
+  }
+
+  public static List<Integer> fibonacci() {
+    List<Integer> fibonacciList = new ArrayList<>();
+
+    int limit = 377;
+    int x = 0;
+
+    fibonacciList.add(0);
+    fibonacciList.add(1);
+
+  // fn = fn -1 + fn -2;
+    while (x < limit) {
+      int ult = fibonacciList.size() - 1;
+      int penult = fibonacciList.size() - 2;
+      fibonacciList.add(fibonacciList.get(ult) + fibonacciList.get(penult));
+      x=fibonacciList.get(ult);
+    }
+
+    fibonacciList.remove(fibonacciList.size() - 1);
+    return fibonacciList ;
+  }
+
+  public static Boolean isFibonacci(Integer a) {
+    return fibonacci().contains(a);
+  }
+
+}
